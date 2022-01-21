80068cb5f459d6a8939a8d17e92db795cfd0ba32
ignacio.laprida <ignacio.laprida.2@gmail.com>
2022-01-21 14:11:03 -0300

    casi listo

--- desafio.js
+++ desafio.js
@@ -1,13 +1,14 @@
 
-
-let bienvenido = prompt("Ingrese nombre").trim()
-
-if (bienvenido !="") {
-alert("Bienvenido a nuestra Tienda " + bienvenido)
-} else {
-    alert("Falta nombre")
+for(i = 0; i < 5; i++) {
+    let bienvenido = prompt("Ingrese nombre").trim().toLocaleUpperCase()
+    if (bienvenido !="") {
+    alert("Bienvenido a nuestra Tienda, " + bienvenido)
+    break}
+    
+    else if (bienvenido == ""){
+        alert("Falta nombre")
+    }
 }
-
 
 let saludo = parseInt(prompt("Somos la mejor tienda de venta de prodcutos de regalos: \nPresione 1 por el producto 1\nPresione 2 por el producto 2\nPresione 3 por el producto 3\nPresione 4 sino queres comprar"))
 
@@ -34,18 +35,18 @@ switch(saludo) {
             alert("No pusiste ninguno de los botones")
             break;
 }
-let producto1 = 10
-let producto2 = 20
-let producto3 = 30 
+let producto1 = 1000
+let producto2 = 2000
+let producto3 = 3000
 
 if(saludo == 1) {
-    alert("El precio de producto 1 es " + producto1)
+    alert("El precio de producto 1 es $" + producto1)
 }
 else if (saludo == 2) {
-    alert("el precio de producto 2 es " + producto2)
+    alert("el precio de producto 2 es $" + producto2)
 }
 else if (saludo == 3) {
-    alert("el precio de producto 3 es " + producto3)
+    alert("el precio de producto 3 es $" + producto3)
 }
 
 let cuota = parseInt(prompt("Como vas a pagar\n"+"1 Efectivo\n"+"2 Cuotas\n"+"3 Cancelar\n"))
@@ -58,7 +59,7 @@ switch(cuota) {
         break
 
     case 2:
-        alert("Abonas en cuotas")
+        alert("Abonas en 12 cuotas con interes de %10")
         break
     
     case 3:
@@ -72,71 +73,41 @@ switch(cuota) {
 
 const suma = (a, b) => a + b
 const resta = (a, b) => a - b
+const div = (a, b) => a / b
 const iva = (x) => x * 0.21
-const pagos = x => x / 3
-const recargoCuota = x => x * 0.05
 
 let precioFinalA = suma(producto1, iva(producto1))
 let precioFinalB = suma(producto2, iva(producto2))
 let precioFinalC = suma(producto3, iva(producto3))
-
+let cuotaProductoA = div(precioFinalA, 12)
+let cuotaProductoB = div(precioFinalB, 12)
+let cuotaProductoC = div(precioFinalC, 12)
+let precioTotalCuotaA = suma(cuotaProductoA, 12)
+let precioTotalCuotaB = suma(cuotaProductoB, 12)
+let precioTotalCuotaC = suma(cuotaProductoC, 12)
 
 if(cuota == 1 && saludo == 1) {
-    alert("el precio con iva es de " + precioFinalA)
+    alert("el precio con iva es de $" + precioFinalA)
 } 
 else if (cuota == 1 && saludo == 2) {
-        alert("el precio con iva es de " + precioFinalB)
+        alert("el precio con iva es de $" + precioFinalB)
 }
 else if (cuota == 1 && saludo == 3) {
-        alert("el precio con iva es de " + precioFinalC)
+        alert("el precio con iva es de $" + precioFinalC)
 }
-/*     else if (cuota == 2) {
-    alert("el precio con iva es de " + producto2)
-} */
+else if (cuota == 2 && saludo == 1) {
+    alert("El costo de cada cuota queda en $" + cuotaProductoA).
+    alert("El precio Total seria de $" + precioTotalCuotaA)
+}
+else if (cuota == 2 && saludo == 2) {
+    alert("El costo de cada cuota queda en $" + cuotaProductoB)
+    alert("El precio Total seria de $" + precioTotalCuotaB)
+}
+else if (cuota == 2 && saludo == 3) {
+    alert("El costo de cada cuota queda en $" + cuotaProductoC)
+    alert("El precio Total seria de $" + precioTotalCuotaC)
+}
 
 
-/* let cuota1 = prompt("Como vas a pagar\n"+"1 Efectivo\n"+"2 Cuotas\n"+"3 Cancelar\n"+"4 asdsa")
-if (cuota1 == 1) {
-    alert("El precio de producto 1 es ")
-} */
-
-
-
-/* do {
-    cuota = parseInt(prompt("Como vas a pagar\n"+"1 Efectivo\n"+"2 Cuotas\n"+"3 Cancelar\n"+"4 asdsa"))
-
-
-    switch(cuota) {
-
-        case 1:
-            alert("Abonas en efectivo")
-            break
-
-        case 2:
-            alert("Abonas en cuotas 2")
-            break
-        
-        case 3:
-            alert("Te esperamos para la próxima visita")
-            break
-        
-        case 4:
-            alert("asdasd")
-            break
-
-            default:
-                alert("No pusiste ninguno de los botones")
-                break;
-
-    }
-} while(saludo != 3 ) */
-
-
-/* for(i = 0; i <=3; i++) {
-
-} */
-
-
-/* while() */
 
 

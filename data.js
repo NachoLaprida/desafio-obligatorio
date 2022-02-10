5d1695ce952fd3fb66569327fb789d1d85247ab1
ignacio.laprida <ignacio.laprida.2@gmail.com>
2022-02-10 15:44:26 -0300

    desafio eventos

--- data.js
+++ data.js
@@ -1,8 +1,26 @@
-const products = [
-    {id: 1, name: "Cerveza",       price: 120, stock: 10},
-    {id: 2, name: "Fernet",        price: 80, stock: 10},
-    {id: 3, name: "Vino",          price: 100, stock: 10},
-    {id: 4, name: "Whiskey",       price: 70, stock: 10},
-    {id: 5, name: "Ron",           price: 150, stock: 10},
-    {id: 6, name: "Aguardiente",   price: 95, stock: 10}
+const Products = [
+    {id: 1, name: "Perro",       price: 120, stock: 10, img: "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/6124cf315cafe8c3101f8bab/perro-slide_0.jpg"},
+    {id: 2, name: "Gato",        price: 80, stock: 10, img: "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-marron_0.jpg"},
+    {id: 3, name: "Raton",          price: 100, stock: 10, img: ""},
+    {id: 4, name: "Iguana",       price: 70, stock: 10, img: ""},
+    {id: 5, name: "Erizo",           price: 150, stock: 10, img: ""},
+    {id: 6, name: "Gallina",   price: 95, stock: 10, img: ""}
 ]
+
+const Cart = []
+
+class ProductCart {
+    constructor(obj) {
+        this.id = obj.id
+        this.name = obj.name
+        this.unit_price = obj.price
+        this.img = obj.img
+
+        this.total = this.unit_price
+        this.quantity = 1
+    }
+    add() {
+        this.quantity++
+        this.total += this.unit_price
+    }
+}
--- dom.js
+++ dom.js
@@ -1,20 +1,20 @@
 // editando html
-let texto = document.getElementById("componente")
-texto.innerHTML = "Productos disponibles"
+/* let texto = document.getElementById("componente")
+texto.innerHTML = "Productos disponibles" */
 
 // debes crear un contenedor
-let contenedorTitulo = document.getElementById("newTitulo");
+/* let contenedorTitulo = document.getElementById("newTitulo"); */
 
 //Add titulo
-let titulo2 = document.createElement("div");
+/* let titulo2 = document.createElement("div");
 titulo2.innerHTML = "<h3>Productos Limitados</h3>";
-contenedorTitulo.appendChild(titulo2);
+contenedorTitulo.appendChild(titulo2); */
 // document.texto.append(titulo2);
 
 
 //add productos
 
-const productos = [
+/* const productos = [
     {id: 1, name: "Lampara", price: 120},
     {id: 2, name: "Plato", price: 150},
     {id: 3, name: "Apoya vasos", price: 80},
@@ -40,6 +40,85 @@ for(const producto of productos) {
     
 `
 padre.appendChild(contenedor)
+} */
+
+// debes crear un contenedor
+let ti = document.getElementById("pTienda");
+
+//Add titulo
+let lo = document.createElement("div");
+/* lo.innerHTML = `<h3>Nuestros Productos</h3>`; */
+ti.appendChild(lo);
+// document.texto.append(titulo2);
+
+
+
+const mostrarCarritoCompras = () => {
+    const boxProducts = document.getElementById("productOnCart")
+    let allProducts =""
+    Cart.forEach(producto => {
+        allProducts +=`
+        <div class="card" style="width: 18rem;">
+        <img src="${producto.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${producto.name}</h5>
+            <i>Cantidad: ${producto.quantity}</i>
+            <p class="card-text">Precio por unidad: $ ${producto.unit_price}</p>
+            <p class="card-text">Total: $ ${producto.total}</p>
+        </div>
+    </div>
+    `
+    })
+
+    boxProducts.innerHTML = allProducts
+
+    clickProd()
 }
 
+const funcionProductos = () => {
+    const boxProducts = document.getElementById("listado")
+    let allProducts =""
+    Products.forEach(producto => {
+        allProducts +=`
+            
+            <div class="card" style="width: 18rem;">
+                <img src="${producto.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.name}</h5>
+                    <p class="card-text">$ ${producto.price}</p>
+                    <button class="Agregar btn btn-success" id="ID-${producto.id}">Agregar al Carrito 🛒</button>
+                </div>
+            </div>
+    `
+    })
 
+    boxProducts.innerHTML = allProducts
+
+    clickProd()
+}
+
+const clickProd = () => {
+
+    const btnAddCarts = document.getElementsByClassName("Agregar")
+    for(const btn of btnAddCarts) {
+        btn.onclick = addCart
+    }
+}
+
+const addCart = (e) => {
+    const productId = parseInt(e.target.id.split("-")[1])
+
+    const product = Products.find(p => p.id == productId)
+    const productCart = new ProductCart(product)
+
+    const productInCart = Cart.find(p => p.id == productId)
+    if(productInCart) {
+        productInCart.add()
+    } else {
+        Cart.push(productCart)
+    }
+
+    mostrarCarritoCompras()
+}
+
+funcionProductos()
